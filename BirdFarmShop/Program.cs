9648938248e0a8471debb9d2cb714a6e78a5b5e3
Thaiquocse <Thaiquocse@gmail.com>
2024-10-21 17:01:00 +0700

    Update

--- BirdFarmShop/Program.cs
+++ BirdFarmShop/Program.cs
@@ -12,24 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddRazorPages();
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = "Cookies";
-    options.DefaultChallengeScheme = "Google"; // Hoặc tên của nhà cung cấp xác thực khác
-})
-    .AddCookie("Cookies")
-    .AddGoogle("Google",googleOptions =>
-    {
-        // Đọc thông tin Authentication:Google từ appsettings.json
-        IConfigurationSection googleAuthNSection = builder.Configuration.GetSection("Authentication:Google");
 
-        // Thiết lập ClientID và ClientSecret để truy cập API google
-        googleOptions.ClientId = googleAuthNSection["ClientId"];
-        googleOptions.ClientSecret = googleAuthNSection["ClientSecret"];
-        // Cấu hình Url callback lại từ Google (không thiết lập thì mặc định là /signin-google)
-        googleOptions.CallbackPath = "/CallBack";
-
-    });
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(10);
